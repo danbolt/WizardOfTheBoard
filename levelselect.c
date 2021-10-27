a689aaf6dfd289e08e84f3183935dc8a0157027b
Daniel Savage <dansvg@gmail.com>
2021-10-26 21:44:20 -0700

    Sidebar state

--- levelselect.c
+++ levelselect.c
@@ -24,8 +24,13 @@ static u32 currentlySelectedLevel;
 
 static float timePassed;
 
+static u8 inTheOptionsPanel;
+static float horizontalSwipe;
+
 static u8 downPressed;
 static u8 upPressed;
+static u8 rightPressed;
+static u8 leftPressed;
 static u8 stickInDeadzone;
 
 static float selectedLevelLerpValue;
@@ -46,8 +51,13 @@ void initLevelSelect() {
   slideOutLerpValue = 0.f;
   timePassed = 0.f;
 
+  inTheOptionsPanel = 0;
+  horizontalSwipe = 0.f;
+
   downPressed = 0;
   upPressed = 0;
+  rightPressed = 0;
+  leftPressed = 0;
   stickInDeadzone = 0;
 
   floorIndicatorText[0] = '\0';
@@ -77,6 +87,8 @@ void makeLevelSelectDisplayList() {
 
   guMtxIdent(&dynamicp->modelling);
 
+  const int swipeOffset = (int)horizontalSwipe;
+
   gDPPipeSync(glistp++);
   gDPSetCycleType(glistp++,G_CYC_1CYCLE);
   gDPSetTexturePersp(glistp++, G_TP_NONE);
@@ -92,34 +104,37 @@ void makeLevelSelectDisplayList() {
   gDPLoadTextureBlock(glistp++, OS_K0_TO_PHYSICAL(backgroundTexture), G_IM_FMT_I, G_IM_SIZ_8b, 64, 64, 0, G_TX_NOMIRROR, G_TX_NOMIRROR, 6, 6, G_TX_NOLOD, G_TX_NOLOD);
   gDPSetPrimColor(glistp++, 0, 0, 0x00, 0x33, 0x61, 0xff);
   gSPTextureRectangle(glistp++, (0) << 2, (0) << 2, (SCREEN_WD) << 2, (SCREEN_HT) << 2, 0, ((u32)(timePassed * 1.f * 64.f)) << 5, ((u32)(timePassed * 0.7f * 64.f)) << 5, 1 << 10, 1 << 10);
+  gDPSetPrimColor(glistp++, 0, 0, 0x00, 0x23, 0x41, 0xff);
+  gSPScisTextureRectangle(glistp++, (-100 + ((int)swipeOffset)) << 2, (0) << 2, (-100 + ((int)swipeOffset) + 128) << 2, (SCREEN_HT) << 2, 0, ((u32)(timePassed * 1.f * 64.f)) << 5, ((u32)(timePassed * 0.7f * 64.f)) << 5, 1 << 10, 1 << 10);
 
 
   gDPPipeSync(glistp++);
   gDPLoadTextureBlock(glistp++, OS_K0_TO_PHYSICAL(iconsTexture), G_IM_FMT_IA, G_IM_SIZ_8b, 256, 16, 0, G_TX_NOMIRROR, G_TX_NOMIRROR, 0, 0, G_TX_NOLOD, G_TX_NOLOD);
 
+
   gDPSetPrimColor(glistp++, 0, 0, 0x99 >> 1, 0x42 >> 1, 0x8C >> 1, 0xff);
   for (int i = 0; i < NUMBER_OF_LEVELS; i++) {
     const s32 offset = (int)(((float)i - (selectedLevelLerpValue)) * -16.f);
-    gSPScisTextureRectangle(glistp++, (SCREEN_WD - TITLE_SAFE_HORIZONTAL - 16 - 64) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL - 16 + offset - 32) << 2, (SCREEN_WD - TITLE_SAFE_HORIZONTAL - 16) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL + offset - 32) << 2, 0, ((i % 3) * 64) << 5, 0 << 5, 1 << 10, 1 << 10);
+    gSPScisTextureRectangle(glistp++, (swipeOffset + SCREEN_WD - TITLE_SAFE_HORIZONTAL - 64 - 64) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL - 16 + offset - 32) << 2, (swipeOffset + SCREEN_WD - TITLE_SAFE_HORIZONTAL - 64) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL + offset - 32) << 2, 0, ((i % 3) * 64) << 5, 0 << 5, 1 << 10, 1 << 10);
   }
   gDPSetPrimColor(glistp++, 0, 0, 0x99, 0x42, 0x8C, 0xff);
   for (int i = 0; i < NUMBER_OF_LEVELS; i++) {
     int xOffset = (i == currentlySelectedLevel) ? (int)(slideOutLerpValue) : 0;
     const s32 offset = (int)(((float)i - (selectedLevelLerpValue)) * -16.f);
-    gSPScisTextureRectangle(glistp++, (SCREEN_WD - TITLE_SAFE_HORIZONTAL - 16 - 64 + xOffset) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL - 16 + offset - 32) << 2, (SCREEN_WD - TITLE_SAFE_HORIZONTAL - 16 + xOffset) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL + offset - 32) << 2, 0, ((i % 3) * 64) << 5, 0 << 5, 1 << 10, 1 << 10);
+    gSPScisTextureRectangle(glistp++, (swipeOffset + SCREEN_WD - TITLE_SAFE_HORIZONTAL - 64 - 64 + xOffset) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL - 16 + offset - 32) << 2, (swipeOffset + SCREEN_WD - TITLE_SAFE_HORIZONTAL - 64 + xOffset) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL + offset - 32) << 2, 0, ((i % 3) * 64) << 5, 0 << 5, 1 << 10, 1 << 10);
   }
-  gSPScisTextureRectangle(glistp++, (SCREEN_WD - TITLE_SAFE_HORIZONTAL - 16 - 64) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL - 16 + ((int)((NUMBER_OF_LEVELS - selectedLevelLerpValue) * -16)) - 32) << 2, (SCREEN_WD - TITLE_SAFE_HORIZONTAL - 16 - 48) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL + ((int)((NUMBER_OF_LEVELS - selectedLevelLerpValue) * -16)) - 32) << 2, 0, 192 << 5, 0 << 5, 1 << 10, 1 << 10);
-  gSPScisTextureRectangle(glistp++, (SCREEN_WD - TITLE_SAFE_HORIZONTAL - 16 - 64 + 48) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL - 16 + ((int)((NUMBER_OF_LEVELS - selectedLevelLerpValue) * -16)) - 32) << 2, (SCREEN_WD - TITLE_SAFE_HORIZONTAL - 16 - 48 + 48) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL + ((int)((NUMBER_OF_LEVELS - selectedLevelLerpValue) * -16)) - 32) << 2, 0, 224 << 5, 0 << 5, 1 << 10, 1 << 10);
+  gSPScisTextureRectangle(glistp++, (swipeOffset + SCREEN_WD - TITLE_SAFE_HORIZONTAL - 64 - 64) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL - 16 + ((int)((NUMBER_OF_LEVELS - selectedLevelLerpValue) * -16)) - 32) << 2, (swipeOffset + SCREEN_WD - TITLE_SAFE_HORIZONTAL - 64 - 48) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL + ((int)((NUMBER_OF_LEVELS - selectedLevelLerpValue) * -16)) - 32) << 2, 0, 192 << 5, 0 << 5, 1 << 10, 1 << 10);
+  gSPScisTextureRectangle(glistp++, (swipeOffset + SCREEN_WD - TITLE_SAFE_HORIZONTAL - 64 - 64 + 48) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL - 16 + ((int)((NUMBER_OF_LEVELS - selectedLevelLerpValue) * -16)) - 32) << 2, (swipeOffset + SCREEN_WD - TITLE_SAFE_HORIZONTAL - 64 - 48 + 48) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL + ((int)((NUMBER_OF_LEVELS - selectedLevelLerpValue) * -16)) - 32) << 2, 0, 224 << 5, 0 << 5, 1 << 10, 1 << 10);
 
   const int cursorOffset = (int)(sinf(timePassed * 8.1616f) * 4.f);
   gDPSetPrimColor(glistp++, 0, 0, 0xff, 0xff, 0xff, 0xff);
-  gSPTextureRectangle(glistp++, (SCREEN_WD - TITLE_SAFE_HORIZONTAL - 32 - 64 - 8 - 32 + cursorOffset) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL - 48 + 4) << 2, (SCREEN_WD - TITLE_SAFE_HORIZONTAL - 32 - 64 - 8 - 16 + cursorOffset) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL - 32 + 4) << 2, 0, 208 << 5, 0 << 5, 1 << 10, 1 << 10);
+  gSPTextureRectangle(glistp++, (swipeOffset + SCREEN_WD - TITLE_SAFE_HORIZONTAL - 64 - 64 - 8 - 32 + cursorOffset) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL - 48 + 4) << 2, (swipeOffset + SCREEN_WD - TITLE_SAFE_HORIZONTAL - 64 - 64 - 8 - 16 + cursorOffset) << 2, (SCREEN_HT - TITLE_SAFE_VERTICAL - 32 + 4) << 2, 0, 208 << 5, 0 << 5, 1 << 10, 1 << 10);
 
   gDPPipeSync(glistp++);
   gDPSetPrimColor(glistp++, 0, 0, 0xff, 0xff, 0xff, 0xff);
   gDPLoadTextureBlock_4b(glistp++, OS_K0_TO_PHYSICAL(displayTextTexture), G_IM_FMT_IA, 512, 16, 0, G_TX_NOMIRROR, G_TX_NOMIRROR, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
   sprintf(floorIndicatorText, "FLOOR %01d", (currentlySelectedLevel + 1));
-  renderDisplayText(TITLE_SAFE_HORIZONTAL + 96, 160, floorIndicatorText);
+  renderDisplayText(swipeOffset + TITLE_SAFE_HORIZONTAL + 48, 160, floorIndicatorText);
 
   if (transitioningState != NOT_TRANSITIONING) {
     gDPPipeSync(glistp++);
@@ -178,12 +193,18 @@ void updateInput() {
     downPressed = 1;
   } else if(contdata[0].trigger & D_JPAD) {
     upPressed = 1;
+  } else if(contdata[0].trigger & L_JPAD) {
+    leftPressed = 1;
+  } else if(contdata[0].trigger & R_JPAD) {
+    rightPressed = 1;
   } else {
     upPressed = 0;
     downPressed = 0;
+    leftPressed = 0;
+    rightPressed = 0;
   }
 
-  if (!stickInDeadzone && (contdata[0].stick_y > -7) && (contdata[0].stick_y < 7)) {
+  if (!stickInDeadzone && (contdata[0].stick_y > -7) && (contdata[0].stick_y < 7) && (contdata[0].stick_x > -7) && (contdata[0].stick_x < 7)) {
     stickInDeadzone = 1;
   }
 
@@ -193,6 +214,14 @@ void updateInput() {
       stickInDeadzone = 0;
     } else if (contdata[0].stick_y > 7) {
       downPressed = 1;
+      stickInDeadzone = 0;
+    }
+
+    if (contdata[0].stick_x < -7) {
+      leftPressed = 1;
+      stickInDeadzone = 0;
+    } else if (contdata[0].stick_x > 7) {
+      rightPressed = 1;
       stickInDeadzone = 0;
     }
 
@@ -210,6 +239,19 @@ void updateInput() {
     currentlySelectedLevel = (currentlySelectedLevel + 1) % NUMBER_OF_LEVELS;
     downPressed = 0;
     slideOutLerpValue = 0.f;
+
+    nuAuSndPlayerPlay(SFX_02_NOBODY_BIP);
+  }
+
+  if (leftPressed && (!inTheOptionsPanel)) {
+    inTheOptionsPanel = 1;
+    leftPressed = 0;
+
+    nuAuSndPlayerPlay(SFX_02_NOBODY_BIP);
+  }
+  if (rightPressed && inTheOptionsPanel) {
+    inTheOptionsPanel = 0;
+    rightPressed = 0;
 
     nuAuSndPlayerPlay(SFX_02_NOBODY_BIP);
   }
@@ -232,6 +274,8 @@ void updateLevelSelect() {
   nuContDataGetEx(contdata,0);
 
   timePassed += deltaTimeSeconds;
+
+  horizontalSwipe = lerp(horizontalSwipe, inTheOptionsPanel ? 100 : 20, 0.13f);
 
   updateLevelSelectTransition();
   updateInput();
